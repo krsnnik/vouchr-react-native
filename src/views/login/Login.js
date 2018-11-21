85b218f152ed4741eafd56ce8c3d1977946d066c
Kyle Liu <kylehliu88@gmail.com>
2018-11-20 20:00:56 -0800

    add a container around the login forms

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -6,22 +6,28 @@ import { name as appName } from '../../../app';
 import App from '../../../App';
 import Triangle_up from './Triangle_up';
 
+import { Dimensions } from 'react-native';
+
+let height = Dimensions.get('window').height / 3; //full height
+
 export default class Login extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
         <Logo />
         <Triangle_up />
+        <View style={styles.formContainer}>
           <TextInput
-              style={{height: 50, alignSelf: 'center', width: 280 , borderColor: '#F66358', borderWidth: 1, borderRadius: 5}}
-              placeholder={'     username'}
-              placeholderTextColor={'gray'}
+            style={styles.loginInputs}
+            placeholder={'     username'}
+            placeholderTextColor={'gray'}
           />
           <TextInput
-              style={{height: 50, alignSelf: 'center', width: 280 , borderColor: '#F66358', borderWidth: 1, borderRadius: 5}}
-              placeholder={'     password'}
-              placeholderTextColor={'gray'}
+            style={styles.loginInputs}
+            placeholder={'     password'}
+            placeholderTextColor={'gray'}
           />
+        </View>
       </View>
     );
   }
@@ -31,6 +37,20 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
     justifyContent: 'space-around',
+  },
+  loginInputs: {
+    height: 50,
+    alignSelf: 'center',
+    width: 280,
+    borderColor: '#F66358',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  formContainer: {
+    height: height,
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    alignContent: 'center',
   },
 });
 
