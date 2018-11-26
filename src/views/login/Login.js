ccb832648ca59ff5cb53c474eed1f5a37d5c67de
Kyle Liu <kylehliu88@gmail.com>
2018-11-26 10:35:39 -0800

    added a fetch call, not working yet but pushing to work from laptop

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   Image,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 
 import Logo from './Logo';
@@ -14,10 +15,73 @@ import App from '../../../App';
 import Triangle_up from './Triangle_up';
 
 import { Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
 
 let height = Dimensions.get('window').height / 2.5;
 
 export default class Login extends Component<Props> {
+  static propTypes = {
+    onSuccess: PropTypes.func,
+    clickRegister: PropTypes.func,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      error: {
+        is: false,
+        header: 'Bad Credentials',
+        message: 'Cannot login try again',
+      },
+    };
+    this.clickLogin = this.clickLogin.bind(this);
+  }
+
+  clickLogin() {
+    const { username, password } = this.state;
+    this.login(username, password).then(json => {
+      if (json.error) {
+        this.setState({
+          error: {
+            is: true,
+            header: 'Bad Credentials',
+            message: 'Cannot login try again',
+          },
+        });
+      } else {
+        Alert.alert('SUCCESS');
+        if (this.props.onSuccess) {
+          this.props.onSuccess();
+        }
+      }
+    });
+  }
+
+  async login(username, password) {
+    try {
+      let response = await fetch(
+        'https://api.vouchr.co/accounts/verify_login',
+        {
+          method: 'POST',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: username,
+            password: password,
+          }),
+        },
+      );
+      let responseJson = await response.json();
+      return responseJson;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -34,10 +98,12 @@ export default class Login extends Component<Props> {
             placeholder={'     password'}
             placeholderTextColor={'gray'}
           />
-          <Image
-            style={{ alignSelf: 'center' }}
-            source={require('./go-button.png')}
-          />
+          <TouchableOpacity onPress={this.clickLogin()}>
+            <Image
+              style={{ alignSelf: 'center' }}
+              source={require('./go-button.png')}
+            />
+          </TouchableOpacity>
         </View>
       </View>
     );
