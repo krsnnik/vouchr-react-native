ff051a02ba407e8786d3e02f695630007c48dcf3
Kyle Liu <kylehliu88@gmail.com>
2018-11-29 19:24:06 -0800

    -installed react-native-image-crop-picker. -onPress for the two buttons inside the PictureBox.js are now hooked up to take a picture with the camera and choose a photo from the file sys.

--- src/views/vouch/create/PictureBox.js
+++ src/views/vouch/create/PictureBox.js
@@ -3,6 +3,7 @@ import { View, Image, AppRegistry, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Dimensions } from 'react-native';
 import { Button } from 'react-native-elements';
+import ImagePicker from 'react-native-image-crop-picker';
 
 let width = Dimensions.get('window').width - 20;
 let height = Dimensions.get('window').height / 4.5;
@@ -17,12 +18,31 @@ export default class PictureBox extends Component<Props> {
             title={'Choose Photo'}
             buttonStyle={styles.button}
             titleStyle={styles.buttonTitle}
+            onPress={() =>
+              ImagePicker.openPicker({
+                width: 300,
+                height: 400,
+                cropping: true,
+              }).then(image => {
+                console.log(image);
+              })
+            }
           />
           <Button
             title={'Take Photo'}
             buttonStyle={styles.button}
             titleStyle={styles.buttonTitle}
+            onPress={() =>
+              ImagePicker.openCamera({
+                width: 480,
+                height: 300,
+                cropping: true,
+              }).then(image => {
+                console.log(image);
+              })
+            }
           />
+          <Image />
         </View>
       </View>
     );
