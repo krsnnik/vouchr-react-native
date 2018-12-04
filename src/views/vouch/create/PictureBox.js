4fb6450f6d8f545ffa319f8cccdc3a771938d1db
Kyle Liu <kylehliu88@gmail.com>
2018-12-03 18:19:27 -0800

    The PictureBox is pretty much done now. When the user chooses or takes a photo, the image will now replace the gray box and hide the buttons. There will also be a X icon on the image which will remove the image and reset the state and show the buttons again.

--- src/views/vouch/create/PictureBox.js
+++ src/views/vouch/create/PictureBox.js
@@ -2,29 +2,88 @@ import React, { Component } from 'react';
 import { View, Image, AppRegistry, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Dimensions } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button, Tile } from 'react-native-elements';
 import ImagePicker from 'react-native-image-crop-picker';
 
 let width = Dimensions.get('window').width - 20;
 let height = Dimensions.get('window').height / 4.5;
 
 export default class PictureBox extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageDisplayMode: 'none',
+      imageUri: '',
+      showIconAndButtons: 'flex',
+    };
+  }
+
   render() {
+    const { imageDisplayMode, imageUri, showIconAndButtons } = this.state;
     return (
       <View style={styles.container}>
-        <Icon name="picture-o" size={90} color="gray" style={styles.icon} />
-        <View style={styles.buttonContainer}>
+        {/*This is the image tile, when the form first loads it is hidden, once
+        the user picks an image it will show and replace the background and hide everything else.*/}
+        <Tile
+          containerStyle={{
+            display: imageDisplayMode,
+            height,
+            width,
+          }}
+          imageContainerStyle={{
+            height,
+            width,
+          }}
+          imageSrc={{ uri: imageUri }}
+          iconContainerStyle={{
+            flexDirection: 'column',
+            justifyContent: 'flex-start',
+            alignSelf: 'center',
+            paddingTop: 55,
+          }}
+          icon={{
+            name: 'times-circle',
+            type: 'font-awesome',
+            onPress: () =>
+              this.setState({
+                imageDisplayMode: 'none',
+                showIconAndButtons: 'flex',
+                imageUri: '',
+              }),
+          }}
+        />
+        {/*This is the "image" icon when there's no image selected*/}
+        <Icon
+          name="picture-o"
+          size={90}
+          color="gray"
+          style={{ display: showIconAndButtons, alignSelf: 'center' }}
+        />
+        {/*This view contains the 2 buttons*/}
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            marginHorizontal: 50,
+            display: showIconAndButtons,
+          }}
+        >
           <Button
             title={'Choose Photo'}
             buttonStyle={styles.button}
             titleStyle={styles.buttonTitle}
             onPress={() =>
               ImagePicker.openPicker({
-                width: 300,
-                height: 400,
+                width,
+                height,
                 cropping: true,
               }).then(image => {
-                console.log(image);
+                this.setState({
+                  imageUri: image.valueOf().path,
+                  imageDisplayMode: 'flex',
+                  showIconAndButtons: 'none',
+                });
+                console.log(height);
               })
             }
           />
@@ -34,15 +93,18 @@ export default class PictureBox extends Component<Props> {
             titleStyle={styles.buttonTitle}
             onPress={() =>
               ImagePicker.openCamera({
-                width: 480,
-                height: 300,
+                width,
+                height,
                 cropping: true,
               }).then(image => {
-                console.log(image);
+                this.setState({
+                  imageUri: image.valueOf().path,
+                  imageDisplayMode: 'flex',
+                  showIconAndButtons: 'none',
+                });
               })
             }
           />
-          <Image />
         </View>
       </View>
     );
@@ -56,9 +118,6 @@ const styles = StyleSheet.create({
     width,
     justifyContent: 'center',
   },
-  icon: {
-    alignSelf: 'center',
-  },
   button: {
     width: 120,
     borderRadius: 100,
@@ -67,11 +126,6 @@ const styles = StyleSheet.create({
   },
   buttonTitle: {
     fontSize: 11,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginHorizontal: 50,
   },
 });
 
